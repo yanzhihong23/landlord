7dfcefbb00a4576df0c06a2250f477ae49f42ad5
Hom <yanzhihong23@qq.com>
2015-03-22 19:01:38 +0800

    add clear-input directive

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -7,6 +7,32 @@ angular.module('landlordApp')
 			templateUrl: 'templates/publicity.html'
 		}
 	})
+	.directive('clearInput', function($parse) {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function(scope, element, attr) {
+        var htmlMarkup = attr.clearBtnMarkup ? attr.clearBtnMarkup : '<i class="ion-ios-trash-outline"></i>';
+        var btn = angular.element(htmlMarkup);
+        element.after(btn);
+
+        btn.on('click', function(event) {
+        	var arr = attr.ngModel.split('.');
+        	if(arr.length === 1) {
+        		scope[arr[0]] = null;
+        	} else if(arr.length === 2) {
+        		scope[arr[0]][arr[1]] = null;
+        	}
+        	scope.$digest();
+        });
+
+        scope.$watch(attr.ngModel, function(val) {
+          var hasValue = val && (val+'').length > 0;
+          btn.css('visibility', hasValue ? 'visible' : 'hidden');
+        });
+      }
+    };
+  })
 	// .directive('autofocus', function($timeout) {
 	//   return {
 	//     restrict: 'A',
